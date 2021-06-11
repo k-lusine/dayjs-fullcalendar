cc49602e4ee820d7eb0b0975974c08ec36765da3
k-lusine <lusinekarapetian@gmail.com>
2021-06-11 23:22:52 +0400

    Try importing fullcalendar/core

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,14 @@
 import day from 'dayjs'
 import { NamedTimeZoneImpl, createPlugin } from '@fullcalendar/common'
+import { Calendar } from '@fullcalendar/core';
 
 class DayNamedTimeZone extends NamedTimeZoneImpl {
+  constructor(timezone) {
+    super(timezone);
+    const name = Calendar.name;
+    console.log(name);
+    
+  }
   offsetForArray(a: number[]): number {
     return (day as any).tz(a, this.timeZoneName).utcOffset()
   }
